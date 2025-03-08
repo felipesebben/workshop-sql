979bd20ae50869349e2ff4432c82ebed5cdf70a8
Felipe Sebben <felipesebben@yahoo.com.br>
2025-03-08 12:03:10 -0300

    feat: add queries and README for lesson 02

--- 02-first_queries/02-first_DQL_queries.sql
+++ 02-first_queries/02-first_DQL_queries.sql
@@ -52,7 +52,7 @@ FROM orders o
 WHERE o.freight > 15
 ORDER BY o.freight DESC, o.ship_country ASC;
 
--- 3.1.1 Operator '<'
+-- 3.1.2 Operator '<'
 -- Question: In which orders was the applied discount lower than 10%? 
 
 SELECT
@@ -64,7 +64,7 @@ FROM order_details od
 WHERE od.discount < 0.1
 ORDER BY od.discount DESC ,od.quantity DESC, od.unit_price DESC;
 
--- 3.1.1 Operator '<='
+-- 3.1.3 Operator '<='
 -- Question: Which products have less or equal units on order than in stock? 
 
 SELECT 
@@ -77,7 +77,7 @@ FROM products p
 WHERE p.units_in_stock <= units_on_order
 ORDER BY stock_order_difference DESC;
 
--- 3.1.1 Operator '>='
+-- 3.1.4 Operator '>='
 -- Question: Which products have more or equal units on order than in stock? We want to know
 -- if we have too much products in stock with a low number of orders.
 
@@ -91,8 +91,82 @@ FROM products p
 WHERE p.units_in_stock >= units_on_order
 ORDER BY stock_order_difference DESC, p.units_on_order DESC;
 
--- 3.1.1 Operator '<>'/'!='
+-- 3.1.5 Operator '<>'/'!='
+-- Question: How many employees that are not Sales Representatives are there in the company?
 
--- 3.1.1 Operator '<'
+SELECT 
+COUNT(*)
+FROM employees e
+WHERE UPPER(e.title) != 'SALES REPRESENTATIVE';
 
--- 3.1.1 Operator '<'
+-- 3.1.6 Operator '<'
+-- Question: Which countries do we have less than 2 suppliers?
+
+SELECT
+	s.country, 
+	COUNT(DISTINCT(s.supplier_id)) AS supplier_count
+FROM suppliers s
+GROUP BY s.country
+HAVING COUNT(DISTINCT(s.supplier_id)) < 2;
+
+-- 3.1.7 Operator '>'
+-- Question: Which countries do we have more than 2 suppliers?
+
+SELECT
+	s.country, 
+	COUNT(DISTINCT(s.supplier_id)) AS supplier_count
+FROM suppliers s
+GROUP BY s.country
+HAVING COUNT(DISTINCT(s.supplier_id)) > 2
+ORDER BY supplier_count DESC;
+
+-- 3.2 Combining operators
+-- 3.2.1 '<' AND '>='
+-- Question: Which products have a unit price between 50 and 100?
+SELECT
+	p.product_id,
+	p.product_name,
+	p.unit_price
+FROM products p
+WHERE p.unit_price BETWEEN 50 AND 100 -- Can also be written as p.unit_price >= 50 AND p.unit_price <= 100
+ORDER BY p.unit_price DESC;
+
+-- 3.3 Using 'IS NULL'/'IS NOT NULL'
+-- Question: Which suppliers have their region field null?
+SELECT
+	s.company_name,
+	s.city,
+	s.country,
+	s.region
+FROM suppliers s
+WHERE s.region IS NULL
+ORDER BY s.country ASC;
+
+-- 3.3 Using 'LIKE'
+-- Question: Which customers' names have their initial letter as an "R"?
+SELECT
+	c.contact_name
+FROM customers c
+WHERE UPPER(c.contact_name) LIKE 'R%'
+ORDER BY c.contact_name ASC;
+
+-- 4. AGGREGATE FUNCTIONS
+-- Question: what are some of the statistics we can get from our unit prices?
+
+SELECT
+	COUNT(*) AS n_products,
+	MIN(p.unit_price) AS min_unit_price,
+	MAX(p.unit_price) AS max_unit_price,
+	ROUND(AVG(CAST(p.unit_price AS NUMERIC)),2) AS avg_unit_price,
+	MODE() WITHIN GROUP (ORDER BY p.unit_price) AS mode_unit_price,
+	PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY p.unit_price) AS quartile_25, 
+    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY p.unit_price) AS quartile_50, 
+	PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY p.unit_price) AS quartile_75,
+	(PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY p.unit_price)
+	- PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY p.unit_price)) AS IQR,
+	ROUND(STDDEV_POP(CAST(p.unit_price AS NUMERIC)),2) AS std_dev_unit_price,
+	ROUND(VAR_POP(CAST(p.unit_price AS NUMERIC)),2) AS variance_unit_price
+FROM products p;
+
+
+
--- 02-first_queries/README.md
+++ 02-first_queries/README.md
@@ -1,28 +1,20 @@
 
-# 01 | First Queries <!-- omit from toc -->
+# 02 | First Queries <!-- omit from toc -->
 
 
 - [Introduction](#introduction)
 - [Goals](#goals)
-- [Creating a Database](#creating-a-database)
-- [Creating a Database](#creating-a-database-1)
-- [First Queries, Basic Operations](#first-queries-basic-operations)
+- [Notes about the lesson](#notes-about-the-lesson)
 ## Introduction
-In this introductory lesson, we installed the main applications and configured the databases that will be used throughout the course.
+In this second lesson, we focused on unveiling the main DQL commands, how they behave, their applications and some of their particularities.
 
 ## Goals
 Our main goals were:
-- Understand Postgres and how it works as a relational database management system (RDBMS)
-- Understand the concept of server/client
-- Get acquainted with PostgreSQL and pgAdmin and understand how they work using the concepts of server and client
-- Install all the necessary softwares and set up the database.
-- Create a dummy database using the `Northwind` sample.
-- Perform some introductory queries.
+- Understand the main SQL commands
+- Review the concepts behind each of the first DQL commands
+- Apply some basic queries by trying to answer business-oriented questions
 
+## Notes about the lesson
+Although this was a very introductory lesson, I liked to review the basics and remember some particularities. For instance, I forgot that `ROUND()` only works with `NUMERIC` fields and, as such, I had to apply `CAST()` to return a two decimals value.
 
-## Creating a Database
-Here, we can either create a new database using the Postgres that we installed in our local machine. I decided to use Render's free SQL server so that I don't need to rely on localhost connections.
-## Creating a Database
-We created a database using the `Northwind` dummy dataset, provided by Microsoft (thank you!). It is a reliable database that will be used throughout the course.
-## First Queries, Basic Operations
-We also performed a series of basic queries, exploring Data Query Language operations - those will be the main subject of this lesson. For more information, visit the [basic_operations.sql](01-basic_operations.sql) file, which contains a few exercises and simple challenges.
+It was also rewarding to remember some statistics-related aggregate functions such as `PERCENTILE_CONT()`. For mor information, visit the [02-first_DQL_queries.sql](02-first_DQL_queries.sql) file, which contains notes and exercises.
